75834a660065395435cdb2e2817070c8e24f4b30
ctsndtrvls <cornflower20189@gmail.com>
2022-06-26 20:11:48 +0300

    message

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -7,22 +7,35 @@ export default function MainPage() {
             <img className="main-pic" src={"./img/profile-photo.jpg"} alt="main-pic" />
         <h2>Hi, my name is Elena 👋</h2>
         <p><span className="sign">{'>'}</span> I’m a junior <span className="bg">frontend</span> developer</p>
-        <p><span className="sign">{'>'}</span> I know <span className="bg">React/Next.js/Tailwind</span></p>
+        <p><span className="sign">{'>'}</span> I know <span className="bg">HTML&CSS/JavaScript/Tailwind</span></p>
         <p><span className="sign">{'>'}</span> I <span className="bg">fled</span> Russia after the war started </p>
         <p><span className="sign">{'>'}</span> I want <span className="bg">Germany</span> to become my new home 🏠</p>
         <p><span className="sign">{'>'}</span> Außerdem spreche ich fließend <span className="bg">Deutsch</span></p>
         <p><span className="sign">{'>'}</span> I really want to work with <span className="bg">IONE!</span></p>
         <p><span className="sign">{'>'}</span> I think IONE should hire me because I’m highly <span className="bg">motivated</span> and I learn really fast 💨</p>
         <p><span className="sign">{'>'}</span> Check out my projects:</p>
+        </div>
+
+
             <div className="container-projects">
                 <div className="project">
                     <img src={"./img/project-pic-map.svg"} alt="project-weather" className="project-img" />
-                    <p><a href="#"> Guess the weather</a></p>
+                    <p><a href="https://weather.pervova.dev/"> Guess the weather</a></p>
                     <div className="project-decr">App gives you a location <br />
                     You guess the weather</div>
                     <div className="git-link">
                     <img src={"./img/git.svg"} alt="project-weather" height="30px" width="30px" /> 
-                    <a href="https://github.com/ctsndtrvls/">https://github.com/ctsndtrvls/</a>
+                    <a href="https://github.com/ctsndtrvls/guess-temp">https://github.com/ctsndt...</a>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={"./img/fragments-screen.png"} alt="project-weather" className="project-img" />
+                    <p><a href="https://fragments.pervova.dev/">Science fragments</a></p>
+                    <div className="project-decr">This is a place where I collect <br />
+                    all the new science stuff I learn</div>
+                    <div className="git-link">
+                    <img src={"./img/git.svg"} alt="project-weather" height="30px" width="30px" /> 
+                    <a href="https://github.com/ctsndtrvls/science-fr">https://github.com/ctsndt...</a>
                     </div>
                 </div>
                 <div className="project">
@@ -35,25 +48,16 @@ export default function MainPage() {
                     <a href="https://github.com/ctsndtrvls/">https://github.com/ctsndtrvls/</a>
                     </div>
                 </div>
-                <div className="project">
-                    <img src={"./img/project-pic-map.svg"} alt="project-weather" className="project-img" />
-                    <p><a href="#"> Guess the weather</a></p>
-                    <div className="project-decr">App gives you a location <br />
-                    You guess the weather</div>
-                    <div className="git-link">
-                    <img src={"./img/git.svg"} alt="project-weather" height="30px" width="30px" /> 
-                    <a href="https://github.com/ctsndtrvls/">https://github.com/ctsndtrvls/</a>
+            </div>
+            <div className="container">
+                <div className="recommendation">
+                    <p><span className="sign">{'>'}</span> Recommendation from my mentor:</p>
+                    <p className="line">Elena is great bla bla write more text lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                    <p>Ivan</p>
                     </div>
+                    <div className="end">
+                    <p><span className="sign">{'>'}</span> Thanks for your attention! 🙌</p>
                 </div>
-            </div>  
-            <div className="recommendation">
-              <p><span className="sign">{'>'}</span> Recommendation from my mentor:</p>
-              <p className="line">Elena is great bla bla write more text lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-              <p>Ivan</p>
             </div>
-            <div className="end">
-            <p><span className="sign">{'>'}</span> Thanks for your attention! 🙌</p>
-            </div>
-        </div>
         </div>
 }
